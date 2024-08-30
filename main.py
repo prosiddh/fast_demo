11ff4ded2523aec9d5764cbc73154e198ff7fccd
Prasiddh <133637249+prosiddh@users.noreply.github.com>
2024-08-30 19:15:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 app = FastAPI()
 
 # Set up templates directory
-templates = Jinja2Templates(directory="app_demo/templates")
+templates = Jinja2Templates(directory="fast_demo/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def get_form(request: Request):
